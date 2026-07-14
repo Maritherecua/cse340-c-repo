f2dd33ed22c516a543290afa10962a49c1efd748
Maritherecua <mrtrsarroyo@yahoo.com>
2026-07-14 12:43:43 -0700

    Updated the organizations view to include links to individual organization pages and removed the description from the list view. Also added a new organization view to display the organization's details.

--- src/controllers/organizations.js
+++ src/controllers/organizations.js
@@ -1,5 +1,7 @@
 // Import necessary modules
-import { getAllOrganizations } from '../models/organizations.js';
+//import { getAllOrganizations } from '../models/organizations.js';
+import { getAllOrganizations, getOrganizationDetails } from '../models/organizations.js';
+import { getProjectsByOrganizationId } from '../models/projects.js';
 // Controller function to handle the request for the organizations page
 const showOrganizationsPage = async (req, res) => {
   const organizations = await getAllOrganizations();
@@ -7,5 +9,14 @@ const showOrganizationsPage = async (req, res) => {
 
   res.render('organizations', { title, organizations });
 };
+// Controller function for the organization details page
+const showOrganizationDetailsPage = async (req, res) => {
+  const organizationId = req.params.id;
+  const organizationDetails = await getOrganizationDetails(organizationId);
+  const projects = await getProjectsByOrganizationId(organizationId);
+  const title = 'Organization Details';
+
+  res.render('organizationDetails', { title, organizationDetails, projects });
+};
 // Export the controller function
-export { showOrganizationsPage };
+export { showOrganizationsPage, showOrganizationDetailsPage };
