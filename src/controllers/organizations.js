92a67e4a527cbd8167a33685a0b1c84ef93228eb
Maritherecua <mrtrsarroyo@yahoo.com>
2026-07-13 19:56:32 -0700

    Fixed organizations controller and server setup

--- src/controllers/organizations.js
+++ src/controllers/organizations.js
@@ -1,3 +1,6 @@
+// Import necessary modules
+import { getAllOrganizations } from '../models/organizations.js';
+// Controller function to handle the request for the organizations page
 const showOrganizationsPage = async (req, res) => {
   const organizations = await getAllOrganizations();
   const title = 'Our Partner Organizations';
