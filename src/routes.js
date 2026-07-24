418f674dfa8420e212508d3279b1f55e21d4e808
Maritherecua <mrtrsarroyo@yahoo.com>
2026-07-24 12:35:26 -0700

    Created assign-categories.ejs view to allow users to assign categories to items. Updated routes.js to include a new route for the assign categories page.

--- src/routes.js
+++ src/routes.js
@@ -6,6 +6,8 @@ import { showProjectsPage } from './controllers/projects.js';
 import { showNewProjectForm } from './controllers/projects.js';
 import { processNewProjectForm } from './controllers/projects.js';
 import { showCategoriesPage } from './controllers/categories.js';
+import { showAssignCategoriesForm } from './controllers/projects.js';
+import { processAssignCategoriesForm } from './controllers/projects.js';
 import { testErrorPage } from './controllers/errors.js';
 import { showOrganizationDetailsPage } from './controllers/organizations.js';
 import { showProjectDetailsPage } from './controllers/projects.js';
@@ -29,6 +31,10 @@ router.get('/categories', showCategoriesPage);
 router.get('/organization/:id', showOrganizationDetailsPage);
 router.get('/project/:id', showProjectDetailsPage);
 router.get('/category/:id', showCategoryDetailsPage);
+// Routes to handle the assign categories to project form
+router.get('/assign-categories/:projectId', showAssignCategoriesForm);
+router.post('/assign-categories/:projectId', processAssignCategoriesForm);
+
 // error-handling routes
 router.get('/test-error', testErrorPage);
 //Router to display the form for creating a new organization
